6f27c4c508fb7fce275d67680ce9e880f0c07cd8
Precious Adedibu <adedibuprecious@gmail.com>
2023-03-22 08:38:39 +0100

    update

--- components/CarDetails/Gallery/ExteriorThreeSixty.tsx
+++ components/CarDetails/Gallery/ExteriorThreeSixty.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 
 const ExteriorThreeSixty = () => {
-  const basePath = "https://fastly-production.24c.in/webin/360";
+  const basePath = "/assets/360";
   return (
     <div className="font-roboto">
       <div className=" fixed top-0 left-0 right-0 bottom-0 h-[100vh] w-[100vw] z-50">
@@ -11,7 +11,7 @@ const ExteriorThreeSixty = () => {
            // @ts-ignore
             amount={75}
             imagePath={basePath}
-            fileName="output_{index}.jpeg"
+            fileName="output_{index}.webp"
           />
         </div>
       </div>
