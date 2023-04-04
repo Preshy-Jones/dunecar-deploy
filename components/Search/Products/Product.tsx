bdbed18043ab097167c79854cc3e37e7bcc4dbb5
Preshy-Jones <adedibuprecious@gmail.com>
2023-04-04 20:28:25 +0100

    fixes

--- components/Search/Products/Product.tsx
+++ components/Search/Products/Product.tsx
@@ -21,7 +21,7 @@ const Product = ({ car }) => {
         <ImageSlider
           navigatorOpen={navigatorOpen}
           setnavigatorOpen={setnavigatorOpen}
-          carId={car.id}
+          carId={car._id}
         />
         <Link href={`car-details/${car._id}`} passHref className="w-full">
           <a target="_blank" className="w-full">
@@ -30,11 +30,15 @@ const Product = ({ car }) => {
                 <h2 className="text-[#414141] tracking-primary leading-primary text-[10px] mb-2">
                   DUNE CERTIFIED
                 </h2>
-                <div className="flex mb-2">
-                  <h2 className="1.5xl:text-[1rem] tablet:text-[0.8rem] text-specialRed leading-primary font-normal cursor-pointer h-8">
-                    {car.title}
-                  </h2>
-                  <h2 className="text-xlg">Nigerian Used</h2>
+                <div className="flex mb-3 items-center">
+                  <div className="w-[60%] h-8 flex items-center">
+                    <h2 className="1.5xl:text-[1rem] tablet:text-[0.8rem] text-specialRed leading-primary font-normal cursor-pointer  break-words ">
+                      {car.title}
+                    </h2>
+                  </div>
+                  <div className="bg-greyBg flex items-center px-1 py-1 rounded-sm w-[40%] justify-center">
+                    <h2 className="text-xlg leading-[0.87875rem] tracking-[-0.03em]">Nigerian Used</h2>
+                  </div>
                 </div>
                 <div className="flex 1.5xl:text-[1.210525rem] lg:[1rem] tablet:text-[0.9rem] tex  justify-start text-specialBlack font-semibold leading-tertiary">
                   <h3 className="mr-3">
